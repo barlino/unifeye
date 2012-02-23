646176a73aa95c32875923203814fcaec38bd0da
Barlino Yeh <barlino@gmail.com>
2012-02-23 16:46:53 +0800

    init

---
+++ Classes/ComOtigaUnifeyeModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComOtigaUnifeyeModuleAssets.h"
+
+extern NSData * dataWithHexString (NSString * hexString);
+
+@implementation ComOtigaUnifeyeModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
